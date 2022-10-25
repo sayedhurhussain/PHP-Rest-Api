89ed6fbd1b3be0167eb3623eaa4028d24d18fb94
sayedhurhussain <sayedhurhussain55@gmail.com>
2022-10-25 15:41:39 +0500

    get single post

--- models/Post.php
+++ models/Post.php
@@ -28,11 +28,11 @@ class Post {
             p.body,
             p.author,
             p.created_at
-            FROM 
+        FROM 
             ' . $this->table . ' p
-            LEFT JOIN
+        LEFT JOIN
             categories c ON p.category_id = c.id
-            ORDER By
+        ORDER By
             p.created_at DESC';
 
             // Prepare statement
@@ -42,6 +42,46 @@ class Post {
             $stmt->execute();
 
             return $stmt; 
+    }
+
+    // Get Single post
+    public function read_single() {
+                //Create query
+                $query = 'SELECT 
+                c.name as category_name,
+                p.id,
+                p.category_id,
+                p.title,
+                p.body,
+                p.author,
+                p.created_at
+            FROM 
+                ' . $this->table . ' p
+            LEFT JOIN
+                categories c ON p.category_id = c.id
+            Where
+                p.id = ?
+                LIMIT 0,1';
+
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // Bind ID
+            $stmt->bindParam(1, $this->id);
+
+            // Execute query
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // set properties
+            $this->title = $row['title'];
+            $this->body = $row['body'];
+            $this->author = $row['author'];
+            $this->category_id = $row['category_id'];
+            $this->category_name = $row['category_name'];
+            return $stmt;
+
     }
     
 } 
