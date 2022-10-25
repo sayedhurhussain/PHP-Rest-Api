cf3710cba9313938b9842d0e92dd0a6ccd22c16d
sayedhurhussain <sayedhurhussain55@gmail.com>
2022-10-25 17:56:00 +0500

    delete post

--- models/Post.php
+++ models/Post.php
@@ -162,4 +162,27 @@ class Post {
                 return false;
         }
     
+        //Delete Post
+        public function delete() {
+            // Create query
+            $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // clean data
+            $this->id = htmlspecialchars(strip_tags($this->id));
+
+            // Bind data
+            $stmt->bindParam(':id', $this->id);
+
+            // Execute query
+            if($stmt->execute()) {
+                return true;
+            }
+            // Print error if something goes wrong
+            printf("Error: %s.\n", $stmt->error);
+
+            return false;
+        }
 } 
