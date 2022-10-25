4a2a854ae43e60c4960d9785a1072809b72bf534
sayedhurhussain <sayedhurhussain55@gmail.com>
2022-10-25 16:09:00 +0500

    create post

--- models/Post.php
+++ models/Post.php
@@ -83,5 +83,41 @@ class Post {
             return $stmt;
 
     }
+
+    // Create Post
+    public function create() {
+        // Create query
+        $query = 'INSERT INTO ' . 
+            $this->table . '
+        SET
+            title = :title,
+            body = :body,
+            author = :author,
+            category_id = :category_id';
+
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // clean data
+            $this->title = htmlspecialchars(strip_tags($this->title));
+            $this->body = htmlspecialchars(strip_tags($this->body));
+            $this->author = htmlspecialchars(strip_tags($this->author));
+            $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+            // Bind data
+            $stmt->bindParam(':title', $this->title);
+            $stmt->bindParam(':body', $this->body);
+            $stmt->bindParam(':author', $this->author);
+            $stmt->bindParam(':category_id', $this->category_id);
+
+            // Execute query
+            if($stmt->execute()){
+                return true;
+            }
+            // Print error if something goes wrong
+            printf("Error: %s.\n", $stmt->error);
+
+            return false;
+    }
     
 } 
