bf74cbbc6d7ab36b4bdf4119ea0220d2bff308e8
sayedhurhussain <sayedhurhussain55@gmail.com>
2022-10-25 17:19:07 +0500

    update post

--- models/Post.php
+++ models/Post.php
@@ -119,5 +119,47 @@ class Post {
 
             return false;
     }
+
+        // Update Post
+        public function update() {
+            // Create query
+            $query = 'UPDATE ' . 
+                $this->table . '
+            SET
+                title = :title,
+                body = :body,
+                author = :author,
+                category_id = :category_id
+            WHERE
+                id = :id';
+
+    
+                // Prepare statement
+                $stmt = $this->conn->prepare($query);
+    
+                // clean data
+                $this->title = htmlspecialchars(strip_tags($this->title));
+                $this->body = htmlspecialchars(strip_tags($this->body));
+                $this->author = htmlspecialchars(strip_tags($this->author));
+                $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+                $this->id = htmlspecialchars(strip_tags($this->id));
+    
+                // Bind data
+                $stmt->bindParam(':title', $this->title);
+                $stmt->bindParam(':body', $this->body);
+                $stmt->bindParam(':author', $this->author);
+                $stmt->bindParam(':category_id', $this->category_id);
+                $stmt->bindParam(':id', $this->id);
+                
+    
+                // Execute query
+                if($stmt->execute()) {
+                    return true;
+                }
+                // Print error if something goes wrong
+                printf("Error: %s.\n", $stmt->error);
+    
+                return false;
+        }
     
 } 
